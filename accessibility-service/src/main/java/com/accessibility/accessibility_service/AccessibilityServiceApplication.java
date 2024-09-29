a524a03690e1d6e90df18abc55c39f7b025cdccb
Joel Obowu <57295185+bedbugs-bit@users.noreply.github.com>
2024-09-29 22:26:16 +0300

    Create project structure

---
+++ accessibility-service/src/main/java/com/accessibility/accessibility_service/AccessibilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.accessibility.accessibility_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessibilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessibilityServiceApplication.class, args);
+	}
+
+}
---
+++ accessibility-service/src/test/java/com/accessibility/accessibility_service/AccessibilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accessibility.accessibility_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessibilityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
